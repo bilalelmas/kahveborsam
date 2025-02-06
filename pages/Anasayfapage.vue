e02e67b8044a568069a3efb1f1b4fe6d1428a1cb
Bilal Elmas <bilaelmas@hotmail.com>
2025-02-06 21:51:35 +0300

    AnasayfaPage

--- pages/Anasayfapage.vue
+++ pages/Anasayfapage.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <Carousel />
     <ContainerComponent />
     <Box />
     <Product />
@@ -14,6 +15,7 @@ import Box from '~/components/Box.vue';
 import Product from '~/components/Product.vue';
 import BannerComponent from '~/components/BannerComponent.vue';
 import Footer from '~/components/Footer.vue';
+import Carousel from '~/components/Carousel.vue';
 
 // Sayfa meta verisi (default layout kullanılıyor)
 definePageMeta({
